621520d4d4b387d00672c006eaf7f198e0eb04b3
Rachel <nershly@gmail.com>
2019-09-11 13:08:53 +0100

    Implement Faction and Weapon Runemark selection

--- scripts/main.js
+++ scripts/main.js
@@ -101,6 +101,22 @@ drawWeapon = function(weaponDiv, pixelPosition) {
     writeScaled(
         damageBase + "/" + damageCrit, 
         {x: pixelPosition.x + 421, y: statsPosY});
+
+    var runemark = getSelectedRunemark(weaponDiv.find("#weaponRunemarkSelect")[0]); 
+    if (runemark != null)
+    {
+        var position = scalePixelPosition({x: pixelPosition.x + 30, y: pixelPosition.y + 40});
+        var size = scalePixelPosition({x: 80, y: 80});
+        getContext().drawImage(runemark, position.x, position.y, size.x, size.y);
+        //var img = new Image();
+        //img.onload = function() {
+        //    var position = scalePixelPosition({x: pixelPosition.x + 30, y: pixelPosition.y + 40});
+        //    var size = scalePixelPosition({x: 80, y: 80});
+        //
+        //    getContext().drawImage(img, position.x, position.y, size.x, size.y);
+        //};
+        //img.src = runemark.src;
+    }
 }
 
 function getWeapon(weaponId) {
@@ -115,8 +131,54 @@ function getWeapon2() {
     return getWeapon("#weapon2");
 }
 
+function getLabel(element)
+{
+    return $(element).prop("labels")[0];
+}
+
+function getImage(element)
+{
+    return $(element).find("img")[0];
+}
+
+function getSelectedRunemark(radioDiv) {
+    var checked = $(radioDiv).find('input:checked');
+    if (checked.length > 0)
+    {
+        return getImage(getLabel(checked[0]));
+    }
+}
+
+function getSelectedFactionRunemark() {
+    return getSelectedRunemark($('#factionRunemarkSelect')[0]);
+}
+
 render = function() {
     drawBackground();
+
+    var runemark = getSelectedFactionRunemark(); 
+    
+    if (runemark != null)
+    {
+       var position = scalePixelPosition({x: 45, y: 45});
+       var size = scalePixelPosition({x: 80, y: 80});
+    
+       getContext().drawImage(runemark, position.x, position.y, size.x, size.y);
+    }
+
+    // if (runemark != null)
+    // {
+    //     var img = new Image();
+    //     img.onload = function() {
+        
+    //        var position = scalePixelPosition({x: 45, y: 45});
+    //        var size = scalePixelPosition({x: 80, y: 80});
+        
+    //        getContext().drawImage(img, position.x, position.y, size.x, size.y);
+        
+    //     };
+    //     img.src = runemark.src;
+    // }
 
     getContext().font = "30px rodchenkoctt";
     getContext().fillStyle = "white";
@@ -176,6 +238,21 @@ onWeaponMinRangeChanged = function(minRange) {
 onWeaponMaxRangeChanged = function(maxRange) {
     var minRange = $(maxRange.parentNode).find("#rangeMin")[0];
     minRange.value = Math.min(maxRange.value, minRange.value);
+
+    onAnyChange();
+}
+
+onRunemarkSelectionChanged = function(radioButton)
+{
+    var radioSection = radioButton.parentNode.parentNode;
+    var allRadioButtons = $('input', radioSection);
+
+    for (i = 0; i < allRadioButtons.length; i++)
+    {
+        getImage(getLabel(allRadioButtons[i])).style.backgroundColor = "black";    
+    }
+
+    getImage(getLabel(radioButton)).style.backgroundColor = "darkred"; 
 
     onAnyChange();
 }
