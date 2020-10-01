7d07bcfd157512fc26a92c7a0731910915c24616
Hendrik Berends <hendrikberends@Hendriks-MacBook-Pro.local>
2020-10-01 22:45:51 +0200

    stuck with new weapon-profile image, now adjusting positions

--- assets/js/main.js
+++ assets/js/main.js
@@ -477,16 +477,16 @@ render = function(fighterData) {
 
     if (fighterData.weapon1.enabled && fighterData.weapon2.enabled)
     {
-        drawWeapon(fighterData.weapon1, {x: 50, y: 750}); // Default was x:29, y:397
-        drawWeapon(fighterData.weapon2, {x: 50, y: 950}); // Default was x:29, y:564
+        drawWeapon(fighterData.weapon1, {x: 50, y: 775}); // Default was x:29, y:397
+        drawWeapon(fighterData.weapon2, {x: 50, y: 975}); // Default was x:29, y:564
     }
     else if (fighterData.weapon1.enabled)
     {
-        drawWeapon(fighterData.weapon1, {x: 50, y: 850}); // Default was x:29, y:463
+        drawWeapon(fighterData.weapon1, {x: 50, y: 875}); // Default was x:29, y:463
     }
     else if (fighterData.weapon2.enabled)
     {
-        drawWeapon(fighterData.weapon2, {x: 50, y: 850}); // Default was x:29, y:463
+        drawWeapon(fighterData.weapon2, {x: 50, y: 875}); // Default was x:29, y:463
     }
     for (i = 0; i < fighterData.tagRunemarks.length; i++)
     {
