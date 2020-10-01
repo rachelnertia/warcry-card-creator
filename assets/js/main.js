8f7fd32b0c087bc6f98872ef1602bd6c0322b734
Hendrik Berends <hendrikberends@Hendriks-MacBook-Pro.local>
2020-10-01 22:56:07 +0200

    reduced font-size by 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -457,7 +457,7 @@ render = function(fighterData) {
     drawModel(fighterData.imageUrl, fighterData.imageProperties);
     drawFactionRunemark(fighterData.factionRunemark);
 
-    getContext().font = "90px rodchenkoctt";
+    getContext().font = "88px rodchenkoctt";
     getContext().fillStyle = "white";
     getContext().textBaseline = "top";
     getContext().textAlign = "left";
